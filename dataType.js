6f836614c9c7c7bc5e7261778984b9f1b122e309
raman <kumarraman9934057@gmail.com>
2024-03-12 20:35:31 +0530

    variable

---
+++ dataType.js
@@ -0,0 +1,22 @@
+"use strict"//treat all JS code as newer version
+
+
+
+let name="raman"
+let age=18
+let isLoggedIn=false
+let state;
+
+//number=>2 power 56
+//bigint=>larger value
+//string=""
+//boolean=>true/false
+//null=>standalone value
+//undefine
+//symbol=>unique->generally used during react
+
+
+//object->later on
+
+console.log(typeof null);//is object type
+console.log(typeof undefined)//is indefined type
--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-console.log("sex")
+const id="raman"
+let rolNo= 100
+var college="BIT"
+
+// id="adi"//does not changes
+rolNo=269
+college="bit"
+
+console.table([id,rolNo,college]);
+
+
+
+/*
+Preffer not  to use var varaible
+*/
