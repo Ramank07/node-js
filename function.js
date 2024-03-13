3bc17bf172cdff40c3e92de6bf300f502a52df3a
raman <kumarraman9934057@gmail.com>
2024-03-14 00:45:03 +0530

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function user(num1,num2){
+    // console.log("raman")
+    // return 9+29;
+    return num1-num2;
+    // console.log("donot execute afer return")
+}
+// const result=user()
+const result=user(2,4)
+console.log(result);
