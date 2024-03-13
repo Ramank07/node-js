ebac92fef6017ee0cbfef26e0c5d9e67affcf9c7
raman <kumarraman9934057@gmail.com>
2024-03-13 16:58:53 +0530

    updated Array

--- Array.js
+++ Array.js
@@ -27,7 +27,7 @@ const arrays= newArr.join()//convert it to string
 // console.log(arrays);
 
 //Slice and splice
-console.log("A ",newArr);
+// console.log("A ",newArr);
 
 const newArr1=newArr.slice(1,4);/*just print the full vlaue without including last one and does not changes original array*/
 // console.log("B",newArr1);
@@ -39,3 +39,23 @@ const newArr2=newArr.splice(1,4);/*just print the full vlaue with including last
 
 
 
+//more function of array
+
+const hero1=["ram","laxman","luv"]
+const hero2=["naagraj","doga","saktiman"]
+
+// hero1.push(hero2);//it push as vector in hero1 [ 'ram', 'laxman', 'luv', [ 'naagraj', 'doga', 'saktiman' ] ]
+// console.log(hero1);
+// const newHero=hero1.concat(hero2);
+// console.log(newHero)//[ 'ram', 'laxman', 'luv', 'naagraj', 'doga', 'saktiman' ]
+
+const allHero=[...hero1,...hero2];
+// console.log(allHero);//[ 'ram', 'laxman', 'luv', 'naagraj', 'doga', 'saktiman' ]
+
+const arrOnArr=[1,3,4,[4,4,52],34,[23,24,[344,242]]]
+const  oneArr=arrOnArr.flat(Infinity)
+console.log(oneArr);/*[
+    1,  3,  4,  4,   4,
+   52, 34, 23, 24, 344,
+  242
+]*/
